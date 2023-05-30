1229817ca50e92066bc31b11f5d534c49a562eed
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-05-30 22:58:49 +0700

    styling favorite section

--- iAlbooms/ContentView.swift
+++ iAlbooms/ContentView.swift
@@ -10,12 +10,60 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack{
+                Text("Dengarkan")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+        
+                Button{
+                    
+                }label: {
+                    Image(systemName: "person.circle")
+                        .font(Font.system(.largeTitle)).foregroundColor(Color.red)
+                    
+                }
+            }
+            
+            Button{
+                
+            }label: {
+                Group{
+                    Text("Favorit")
+                        .foregroundColor(Color.black)
+                    Image(systemName:"greaterthan" ).foregroundColor(Color.gray).font(.body)
+                }
+                .font(.title2).fontWeight(.semibold)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack (spacing: 15){
+                    ForEach(0..<10) { _ in
+                        Button{
+                            
+                        }label: {
+                            VStack(alignment: .leading){
+                                Image(systemName: "play.rectangle.fill")
+                                    .resizable()
+                                    .frame(width: 150.0, height: 100.0)
+                                    .foregroundColor(Color.red)
+                                    .cornerRadius(10)
+                                Text("Nama Album")
+                                    .font(.headline)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color.black)
+                                Text("Artist")
+                                    .foregroundColor(Color.gray)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            Spacer()
+        }.padding(.horizontal)
     }
 }
 
