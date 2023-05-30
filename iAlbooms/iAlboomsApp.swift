0b2515be178ae2a59e2f804e46f93f960c02f3bf
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-05-30 20:32:38 +0700

    Initial Commit

---
+++ iAlbooms/iAlboomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAlboomsApp.swift
+//  iAlbooms
+//
+//  Created by Muhammad Fakhryan on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iAlboomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
