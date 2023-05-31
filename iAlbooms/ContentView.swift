60101c263ffc629b73f858329c3d9bff1864d7ac
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-05-31 23:55:54 +0700

    styling detail view

--- iAlbooms/ContentView.swift
+++ iAlbooms/ContentView.swift
@@ -11,63 +11,65 @@ struct ContentView: View {
     @State private var showingSheet = false
     
     var body: some View {
-        VStack {
-            HStack{
-                Text("Dengarkan")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-        
-                Button{
-                    showingSheet.toggle()
-                }label: {
-                    Image(systemName: "person.circle")
-                        .font(Font.system(.largeTitle)).foregroundColor(Color.red)
-                    
-                }.sheet(isPresented: $showingSheet) {
-                    ProfileView()
-                }
-            }
-            
-            Button{
-                
-            }label: {
-                Group{
-                    Text("Favorit")
-                        .foregroundColor(Color.black)
-                    Image(systemName:"greaterthan" ).foregroundColor(Color.gray).font(.body)
-                }
-                .font(.title2).fontWeight(.semibold)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            ScrollView(.horizontal, showsIndicators: false){
-                HStack (spacing: 15){
-                    ForEach(0..<10) { _ in
+        NavigationView{
+            ScrollView{
+                VStack {
+                    HStack{
+                        Text("Dengarkan")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        
                         Button{
-                            
+                            showingSheet.toggle()
                         }label: {
-                            VStack(alignment: .leading){
-                                Image(systemName: "play.rectangle.fill")
-                                    .resizable()
-                                    .frame(width: 150.0, height: 100.0)
-                                    .foregroundColor(Color.red)
-                                    .cornerRadius(10)
-                                Text("Nama Album")
-                                    .font(.headline)
-                                    .fontWeight(.regular)
-                                    .foregroundColor(Color.black)
-                                Text("Artist")
-                                    .foregroundColor(Color.gray)
+                            Image(systemName: "person.circle")
+                                .font(Font.system(.largeTitle)).foregroundColor(Color.red)
+                            
+                        }.sheet(isPresented: $showingSheet) {
+                            ProfileView()
+                        }
+                    }
+                    
+                    Button{
+                        
+                    }label: {
+                        Group{
+                            Text("Favorit")
+                                .foregroundColor(Color.black)
+                            Image(systemName:"greaterthan" ).foregroundColor(Color.gray).font(.body)
+                        }
+                        .font(.title2).fontWeight(.semibold)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack (spacing: 15){
+                            ForEach(0..<10) { _ in
+                                NavigationLink(destination: DetailView()){
+                                    VStack(alignment: .leading){
+                                        Image(systemName: "play.rectangle.fill")
+                                            .resizable()
+                                            .frame(width: 150.0, height: 100.0)
+                                            .foregroundColor(Color.red)
+                                            .cornerRadius(10)
+                                        Text("Nama Album")
+                                            .font(.headline)
+                                            .fontWeight(.regular)
+                                            .foregroundColor(Color.black)
+                                        Text("Artist")
+                                            .foregroundColor(Color.gray)
+                                    }
+                                }
                             }
                         }
                     }
-                }
+                    
+                    Spacer()
+                }.padding(.horizontal)
             }
-            
-            Spacer()
-        }.padding(.horizontal)
+        }
     }
 }
 
---
+++ iAlbooms/DetailView.swift
@@ -0,0 +1,97 @@
+//
+//  DetailView.swift
+//  iAlbooms
+//
+//  Created by Muhammad Fakhryan on 31/05/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        NavigationView{
+            ScrollView(.vertical){
+                VStack{
+                    Group{
+                        Image(systemName: "play.rectangle.fill")
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .cornerRadius(20)
+                        Text("Nama Album")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.bottom, 1.0)
+                            .padding(.top, 2.0)
+                        
+                        Text("Nama Band")
+                            .font(.headline)
+                            .fontWeight(.regular)
+                            .padding(.vertical, 2.0)
+                        HStack{
+                            Text("Genre")
+                                .font(.footnote)
+                            
+                            Text("Tahun")
+                                .font(.footnote)
+                        }.padding(.vertical, 2.0)
+                    }.padding(.vertical, 0)
+                    
+                    Text("Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet")
+                        .padding(.horizontal)
+                        .padding(.bottom, 10.0)
+                        .font(.caption)
+                    
+                    Group{
+                        Divider()
+                        
+                        ForEach(1..<10){ i in
+                            HStack(spacing: 15){
+                                Text("\(i)")
+                                    .foregroundColor(Color.gray)
+                                Text("Judul Lagu")
+                                    .fontWeight(.regular)
+                            }.frame(alignment: .leading)
+                                .padding(.leading)
+                            
+                            if i != 10 - 1{
+                                Divider()
+                                    .padding(.leading, 40.0)
+                            }
+                        }
+                        
+                        Divider()
+                        
+                        Group{
+                            Text("dd MM yyy")
+                            Text("jumlah, durasi")
+                        }
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                        .padding(.leading)
+                        .padding(.vertical, 2.0)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    
+                    Spacer()
+                }
+            }
+        }
+        .toolbar{
+            ToolbarItem(placement: .primaryAction){
+                Button{
+
+                }label:{
+                    Image(systemName: "star.circle.fill")
+                        .foregroundColor(Color.black)
+                }
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
